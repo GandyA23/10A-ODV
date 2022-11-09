fb7b19304c8c87e8876905433c7f5515fc6a2295
Gandy Esaú Ávila Estrada <20193tn128@utez.edu.mx>
2022-11-09 17:59:53 -0600

    Se pinta rayo con Debug para identificar al jugador por la AI

--- Player3D/Assets/Scripts/AI/Sight.cs
+++ Player3D/Assets/Scripts/AI/Sight.cs
@@ -6,11 +6,12 @@ public class Sight : MonoBehaviour
 {
     private bool playerSaw;
     private Transform playerTransform;
+    private SphereCollider visionRange;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        visionRange = GetComponent<SphereCollider>();
     }
 
     // Update is called once per frame
@@ -26,7 +27,32 @@ public class Sight : MonoBehaviour
 
         float dotProduct = Vector3.Dot(transform.forward, vectorToPlayer);
 
-        playerSaw = dotProduct > 0;
+        if (dotProduct > 0)
+        {
+            // Aumenta 1 unidad el rayo en altura para que no choque con el suelo
+            Vector3 origin = transform.position + Vector3.up;
+
+            // Verifica si el objeto en el cual colisiono, es un jugador
+            if (Physics.Raycast(origin, vectorToPlayer, out RaycastHit hit, visionRange.radius))
+            {
+                if (hit.transform.tag == "Player")
+                {
+                    // Dibuja un rayo de color rojo en caso de que colisione con el jugador
+                    Debug.DrawRay(origin, vectorToPlayer, Color.red);
+                    playerSaw = true;
+                }
+                else
+                {
+                    // Dibuja un rayo de color blanco en caso de que colisione con un objeto que no sea el jugador
+                    Debug.DrawRay(origin, vectorToPlayer, Color.white);
+                    playerSaw = false;
+                }
+            }
+        }
+        else 
+        {
+            playerSaw = false;
+        }
 
         Debug.Log(dotProduct);
     }
